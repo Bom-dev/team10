1add043149f464b33f307439573be5ac03085ecf
Jeff <mrjeffrey1011@gmail.com>
2022-10-01 14:47:56 -0400

    installed tailwindcss for quiz

---
+++ team-ten/src/Quiz.js
@@ -0,0 +1,13 @@
+function Quiz() {
+    return (
+      <div>
+            <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+      </div>
+    );
+  }
+  
+  export default Quiz;
+  
